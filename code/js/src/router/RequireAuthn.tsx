deaa086b3716cae690ba242443abaac47d72b014
alexmadeira23 <alexandrealemaomadeira@gmail.com>
2022-12-26 19:36:28 +0000

    fixed require auth

--- code/js/src/router/RequireAuthn.tsx
+++ code/js/src/router/RequireAuthn.tsx
@@ -8,29 +8,35 @@ import { askService, Result } from '../service/askService'
 import { service } from './App'
 import { useCurrentUser, useSetUser } from './Authn'
 
+type Authentication = "error" | "success" | undefined
+
 export function RequireAuthn({ children }: { children: React.ReactNode }): React.ReactElement {
-    console.log("running")
     const location = useLocation()
     const [cookies, setCookie, removeCookie] = useCookies();
 
     const currentUser = useCurrentUser()
     const setCurrentUser = useSetUser()
 
+    const [authentication, setAuthentication]: [Authentication, Dispatch<React.SetStateAction<Authentication>>] = useState(undefined)
+
     const tokenInCookie = cookies["token"]
 
     const token = currentUser ? currentUser.token : tokenInCookie
-    
-    const [user, setUser] : [CurrentUser | undefined,  Dispatch<React.SetStateAction<CurrentUser>>]= useState(undefined)
-    
-    const userHome = askService(service, service.userHome, token)
 
     useEffect(() => {
-        console.log("user: " + user)
-        if (user && !currentUser)
-            setCurrentUser(user)
-    }, [user])
+        console.log("useEffect")
+        service.userHome(token)
+            .then((userHome: UserHome) => {
+                if (!currentUser)
+                    setCurrentUser({token: token, name: userHome.name})
+                setAuthentication("success")
+            })
+            .catch(() => {
+                setAuthentication("error")
+            })
+    }, [])
 
-    if (!userHome) {
+    if (!authentication) {
         return (
             <div>
                 ...loading...
@@ -38,11 +44,11 @@ export function RequireAuthn({ children }: { children: React.ReactNode }): React
         )
     }
 
-    if (userHome.kind == "success") {
-        setUser({token: token, name: userHome.result.name})
+    if (authentication == "success") {
         return <>{children}</>
     } else {
-        removeCookie("token")
+        if (tokenInCookie) 
+            removeCookie("token")
         return <Navigate to="/login" state={{source: location.pathname}} replace={true}/>
     } 
 }
